a4b7e439a4ca2dfcd146312421a2eeb151c72c06
Jeremy Adams <jeremy.adams@ga4gh.org>
2020-08-11 13:12:33 -0400

    attempt to run gh-openapi-docs

--- scripts/build-and-push-docs.sh
+++ scripts/build-and-push-docs.sh
@@ -1,24 +1,20 @@
 NODE_VERSION="12.18.3"
 
-echo -e "Start of Script"
-
-# INSTALL AND LOAD NVM
+# INSTALL NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-echo -e "Has NVM been installed/loaded?"
-nvm
 nvm --version
 
-echo -e "Version of Node:"
+# INSTALL NODEJS
 nvm install ${NODE_VERSION}
 nvm use ${NODE_VERSION}
 node -v
-echo -e "Version of NPM:"
-npm -v
-# npm install -g @redocly/openapi-cli && npm install -g redoc-cli
-# npm install -g @ga4gh/gh-openapi-docs
-# gh-openapi-docs
-echo -e "End of Script"
+
+# INSTALL GH-OPENAPI-DOCS
+npm install -g @redocly/openapi-cli && npm install -g redoc-cli
+npm install -g @ga4gh/gh-openapi-docs
+
+# RUN GH-OPENAPI-DOCS
+gh-openapi-docs
