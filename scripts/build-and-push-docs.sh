13a425655bc80293b73bde5670daa0f8f5e0a7e6
Jeremy Adams <jeremy.adams@ga4gh.org>
2020-08-12 08:45:05 -0400

    added Auth

--- scripts/build-and-push-docs.sh
+++ scripts/build-and-push-docs.sh
@@ -1,5 +1,16 @@
 NODE_VERSION="12.18.3"
 
+function setup_github_account {
+    git config user.name $GH_NAME
+    git config user.email $GH_EMAIL
+    git config credential.helper "store --file=.git/credentials"
+    echo "https://${GH_TOKEN}:x-oauth-basic@github.com" > .git/credentials
+}
+
+function cleanup {
+  rm .git/credentials
+}
+
 # INSTALL NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
@@ -19,16 +30,23 @@ npm install -g @ga4gh/gh-openapi-docs
 # RUN GH-OPENAPI-DOCS
 gh-openapi-docs
 
-# PUSH TO GH-PAGES BRANCH
+# PULL GH-PAGES BRANCH TO LOCAL
+setup_github_account
 git checkout -b gh-pages
 git branch --set-upstream-to=origin/gh-pages
 git config pull.rebase false
 git pull
 
+# COMMIT GH-OPENAPI-DOCS OUTPUTS 
 git add preview
 git add docs
 git add openapi.json
 git add openapi.yaml
 git commit -m "gh-pages commit"
-git status
+
+# PUSH GH-OPENAPI-DOCS OUTPUTS
 git push
+
+cleanup
+
+exit 0
